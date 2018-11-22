d7468a58095c37d0a98a9e330ef99e12a2f4d936
neyney <neyney10@live.com>
2018-11-22 21:45:57 +0200

    clean up, changed to boaz calculationsV2

--- Main.java
+++ Main.java
@@ -3,39 +3,62 @@ import Geom.Point3D;
 
 public class Main {
 
+	static 	MyCoords m = new MyCoords();
 	public static void main(String[] args)
 	{	
-		MyCoords m = new MyCoords();
-		Point3D b9 = new Point3D(32.103315 ,35.209039,670);
+		testDistance3D();
+		testVector();
+		testAzimuth();
+
+
+
+
+	}
+	
+	public static void testDistance3D() {
+		System.out.println("-------- Distance3D tests ----------");
 		
+		Point3D b9 = new Point3D(32.103315 ,35.209039,670);
 		Point3D humus = new Point3D(32.106352,35.205225,650);
+
 		double dist = m.distance3d(b9, humus);
-		System.out.println(dist);
+		double distByVector = m.vectorNormal(m.vector3D(b9, humus));
+		double distByVectorBoaz = m.vectorNormal2D(m.vector3D(b9, humus));
+		System.out.println("Distance3D: "+ dist + " | distByVector: " + distByVector + " | distByVectorBoaz: " + distByVectorBoaz);
+	}
+	
+	public static void testVector() {
+		System.out.println("-------- Vector tests ----------");
+		
+		Point3D b9 = new Point3D(32.103315 ,35.209039,670);
+		Point3D humus = new Point3D(32.106352,35.205225,650);
 		
 		Point3D v3d = m.vector3D(b9, humus);
-		System.out.println(Math.sqrt(v3d.x()*v3d.x()+v3d.y()*v3d.y() + v3d.z()*v3d.z()));
-		System.out.println("Vector3D: "+v3d);
-		System.out.println("Vector3D converted: "+m.ecef2lla(v3d));
-		System.out.println("check add, should be same as humus: "+m.add(b9, v3d));
-		System.out.println();
-		double b9x = Math.toRadians(humus.x()-b9.x());
-		double b9y = Math.toRadians(humus.y()-b9.y());
-		System.out.println("x: "+b9x+" y:"+b9y+"| "+Math.sin(b9x)*6378137 + " | "+ Math.sin(b9y)* 0.84709*6378137);
-		Point3D vector  = new Point3D(Math.sin(b9x)*6378137, Math.sin(b9y)* 0.84709*6378137,humus.z()-b9.z());
-		System.out.println("Vector by excel:" + vector);
-		System.out.println("check add, should be same as humus: "+m.add(b9, vector));
-		System.out.println(Math.sqrt(vector.x()*vector.x()+vector.y()*vector.y() + vector.z()*vector.z()));
+		System.out.println("vector3D Boaz: "+v3d);
+		b9 = m.add(b9,v3d);
+		System.out.println("humus: "+humus);
+		System.out.println("should be humus: "+b9);
+		
+		b9 = new Point3D(32.103315 ,35.209039,670);
+		v3d = m.vector3D(b9, humus);
+		Point3D bv3d = m.vector3D(b9, humus);
+		System.out.println("Vector3D Boaz: "+bv3d + " | Normal: "+m.vectorNormal2D(bv3d));
+		System.out.println("Vector3D: "+v3d+"| Normal: "+m.vectorNormal(v3d));
+	}
+	
 
-		//		Point3D a = new Point3D(32.17218268, 34.81446402, 13.65040889);
-//		Point3D b = m.geo2ecef(a);
-//		System.out.println(a);
-//		System.out.println(b);
-//		System.out.println(m.ecef2lla(b));
-//		System.out.println(m.computeGeodetic(b));
-//		System.out.println();
-//		Point3D az1 = new Point3D(32,34,600);
-//		Point3D az2 = new Point3D(33,33,700);
-		System.out.println(m.azimuth_elevation_dist(b9, humus)[0]);
+	public static void testAzimuth() {
+		System.out.println("-------- Azimuth tests ----------");
+		
+		Point3D b9 = new Point3D(32.103315 ,35.209039,670);
+		Point3D humus = new Point3D(32.106352,35.205225,650);
+		Point3D az1 = new Point3D(44,34,600);
+		Point3D az2 = new Point3D(44,33.9,633);
+		
+		double[] azi = m.azimuth_elevation_dist(az1, az2);
+		System.out.println(azi[0]+" | " +azi[1] + " | "+azi[2]);
+		azi = m.azimuth_elevation_dist(b9, humus);
+		System.out.println(azi[0]+" | " +azi[1] + " | "+azi[2]);
 		
 
 	}
--- src/coords/MyCoords.java
+++ src/coords/MyCoords.java
@@ -6,154 +6,98 @@ public class MyCoords implements coords_converter{
 
 	private final static double earth_radius = 6378137;//ESTIMATE: 6371000;
 	private final static double earth_lon_norm = 0.84709;
-	private final double a = 6378137; // radius
-	private final double e = 8.1819190842622e-2;  // eccentricity
 
-	private final double asq = Math.pow(a,2);
-	private final double esq = Math.pow(e,2);
-	
+
 	@Override
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
-		
-		Point3D p3d = geo2ecef(gps);//new Point3D(gps);
+
+		Point3D p3d = test(gps);
 		p3d.add(local_vector_in_meter);
-		return ecef2lla(p3d);
+		return test2(p3d);
 	}
 
 	@Override
 	public double distance3d(Point3D gps0, Point3D gps1) { //WORKS
+		//
+		//		double d = earth_radius*Math.sin(Point3D.d2r(gps1.x()-gps0.x()));
+		//		double d2 = earth_radius*earth_lon_norm*Math.sin(Point3D.d2r(gps1.y()-gps0.y()));
+		//		double norm = Math.sqrt((d*d+d2*d2));
 
-		double d = earth_radius*Math.sin(Point3D.d2r(gps1.x()-gps0.x()));
-		double d2 = earth_radius*earth_lon_norm*Math.sin(Point3D.d2r(gps1.y()-gps0.y()));
-		double norm = Math.sqrt((d*d+d2*d2));
-		
-		return norm;
+		double deltaPhi,distance,deltaLamda,phi1,phi2,alphaRad,alphaDeg;
+		deltaLamda = Math.toRadians(gps1.y()) - Math.toRadians(gps0.y());
+		phi1 = Math.toRadians(gps0.x());
+		phi2 = Math.toRadians(gps1.x());
+		deltaPhi = phi2 - phi1;
+		double a = Math.pow(Math.sin(deltaPhi/2),2)+Math.cos(phi1)*Math.cos(phi2)*Math.pow(Math.sin(deltaLamda/2),2);
+		double c = 2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+		distance =  earth_radius * c;
+
+
+
+		return distance;
+	}
+
+	
+
+	public double vectorNormal(Point3D vector) {
+		return Math.sqrt(vector.x()*vector.x()+vector.y()*vector.y()+vector.z()*vector.z());
 	}
 	
-	/**
-	 * Converts from LLA (Geodetic) coordinates to ECEF (XYZ) coordinates.
-	 * @param gps0 Point3D object in Geodetic system.
-	 * @return Point3D object in ECEF system.
-	 */
-	public Point3D geo2ecef(Point3D gps0) {
-		double x0 = earth_radius * Math.cos(Point3D.d2r(gps0.x())) * Math.cos(Point3D.d2r(gps0.y()));
-		double y0 = earth_radius * Math.cos(Point3D.d2r(gps0.x())) * Math.sin(Point3D.d2r(gps0.y()));
-		double z0 = earth_radius * Math.sin(Point3D.d2r(gps0.x()));
+	public double vectorNormal2D(Point3D vector) {
+		return Math.sqrt(vector.x()*vector.x()+vector.y()*vector.y());
+	}
+
+
+	@Override
+	public Point3D vector3D(Point3D gps0, Point3D gps1) {	
+		double x0 = earth_radius*Math.sin(Point3D.d2r(gps1.x()-gps0.x()));
+		double y0 = earth_radius*earth_lon_norm*Math.sin(Point3D.d2r(gps1.y()-gps0.y()));
+		double z0 = gps1.z()-gps0.z();
+		
 		return new Point3D(x0,y0,z0);
 	}
+
+
+
 	
-	/**
-	 * Converts from ECEF (XYZ) coordinates to LLA (Geodetic) coordinates.
-	 * @param gps0 Point3D object in ECEF system.
-	 * @return Point3D object in Geodetic system.
-	 */
-	
-
-	 public Point3D computeGeodetic(Point3D gps) { 
-		  double X = gps.x();
-		  double Y = gps.y();
-		  double Z = gps.z();
-		 
-		  //this.geod = new SimpleMatrix(3, 1); 
-		 
-		  // Radius computation 
-		  double r = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2)); 
-		 
-		  // Geocentric longitude 
-		  double lamGeoc = Math.atan2(Y, X); 
-		 
-		  // Geocentric latitude 
-		  double phiGeoc = Math.atan(Z / Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2))); 
-		 
-		  // Computation of geodetic coordinates 
-		  double psi = Math.atan(Math.tan(phiGeoc) / Math.sqrt(1 - Math.pow(e, 2))); 
-		  double phiGeod = Math.atan((r * Math.sin(phiGeoc) + Math.pow(e, 2) * a 
-		    / Math.sqrt(1 - Math.pow(e, 2)) * Math.pow(Math.sin(psi), 3)) 
-		    / (r * Math.cos(phiGeoc) - Math.pow(e, 2) * a * Math.pow(Math.cos(psi), 3))); 
-		  double lamGeod = lamGeoc; 
-		  double N = a / Math.sqrt(1 - Math.pow(e, 2) * Math.pow(Math.sin(phiGeod), 2)); 
-		  double h = r * Math.cos(phiGeoc) / Math.cos(phiGeod) - N; 
-		 
-		  return new Point3D( Math.toDegrees(phiGeod),Math.toDegrees(lamGeod),h );
-		 } 
-	
-	public Point3D ecef2lla(Point3D ecef){
-		  double x = ecef.x();
-		  double y = ecef.y();
-		  double z = ecef.z();
-
-		  double b = Math.sqrt( asq * (1-esq) );
-		  double bsq = Math.pow(b,2);
-		  double ep = Math.sqrt( (asq - bsq)/bsq);
-		  double p = Math.sqrt( Math.pow(x,2) + Math.pow(y,2) );
-		  double th = Math.atan2(a*z, b*p);
-
-		  double lon = Math.atan2(y,x);
-		  double lat = Math.atan2( (z + Math.pow(ep,2)*b*Math.pow(Math.sin(th),3) ), (p - esq*a*Math.pow(Math.cos(th),3)) );
-		  double N = a/( Math.sqrt(1-esq*Math.pow(Math.sin(lat),2)) );
-		  double alt = p / Math.cos(lat) - N;
-
-		  // mod lat to 0-2pi
-		  
-		  //
-		  lat = Point3D.r2d(lat);
-		  lon = Point3D.r2d(lon);
-		  alt = Point3D.r2d(alt);
-
-		  // correction for altitude near poles left out.
-
-		  return new Point3D(lat, lon, alt);
-		}
-	
-	
-	@Override
-	public Point3D vector3D(Point3D gps0, Point3D gps1) {
-		
-		//System.out.printf("%f, %f ,%f\n", x0, y0, z0);
-		
-		
-		Point3D p0 = geo2ecef(gps0);
-		Point3D p1 = geo2ecef(gps1);
-		
-//		//System.out.println(p0 +" \n" +p1);
-//		System.out.println("Original: "+ gps0);
-//		System.out.println("ECEF:" +p0);
-//		//ecef2geo(p0);
-//		System.out.println("LLA/GEODETIC:" + ecef2lla(p0));
-//		//System.out.println(p0.distance3D(p1)); // CORRECT DISTANCE
-		
-		
-		
-		return new Point3D(p1.x() - p0.x(), p1.y() - p0.y(), p1.z() - p0.z());
-	}
 
 	@Override
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1){
-	        double[] arr = new double[3];
-	        
-	        /*
-	         * deltaLamba =  longitude(gps1) - longitude(gps0) in radians
-	         * phi1 = latitude(gps0) in radians
-	         * phi2 = latitude(gps1) in radians
-	         * alphaRad = the Azimuth angle in radians
-	         * alphaDeg = the Azimuth angle in degrees
-	         */
-	        double deltaLamda,phi1,phi2,alphaRad,alphaDeg ;
-	        deltaLamda = Math.toRadians(gps1.y()) - Math.toRadians(gps0.y());
-	        phi1 = Math.toRadians(gps0.x());
-	        phi2 = Math.toRadians(gps1.x());
+		double[] arr = new double[3];
 
-	        double x = Math.sin(deltaLamda)*Math.cos(phi2);//spilt for checking
-	        double y = (Math.cos(phi1)*Math.sin(phi2))-(Math.sin(phi1)*Math.cos(phi2)*Math.cos(deltaLamda));//split for checking
-	        alphaRad = Math.atan2(x,y);
-	        alphaDeg = Math.toDegrees(alphaRad);
-	        if (alphaDeg<0)//just so it will give positive degree
-	            alphaDeg = 360+alphaDeg;
-	        System.out.println(alphaDeg); //check if you want from here.
-	        arr[0] = alphaDeg;
-	        arr[1] = distance3d(gps0, gps1);
-	        return arr;
-	    }
+		/*
+		 * deltaLamba =  longitude(gps1) - longitude(gps0) in radians
+		 * phi1 = latitude(gps0) in radians
+		 * phi2 = latitude(gps1) in radians
+		 * alphaRad = the Azimuth angle in radians
+		 * alphaDeg = the Azimuth angle in degrees
+		 */
+		double deltaLamda,phi1,phi2,alphaRad,alphaDeg;
+		deltaLamda = Math.toRadians(gps1.y()) - Math.toRadians(gps0.y());
+		phi1 = Math.toRadians(gps0.x());
+		phi2 = Math.toRadians(gps1.x());
+
+		double x = Math.sin(deltaLamda)*Math.cos(phi2);//Split for checking
+		double y = (Math.cos(phi1)*Math.sin(phi2))-(Math.sin(phi1)*Math.cos(phi2)*Math.cos(deltaLamda));//split for checking
+		alphaRad = Math.atan2(x,y);
+		alphaDeg = Math.toDegrees(alphaRad);
+		if (alphaDeg<0)//just so it will give positive degree
+			alphaDeg = 360+alphaDeg;
+
+		arr[0] = alphaDeg;
+		double A = (gps1.z()-gps0.z());
+		double C = distance3d(gps0,gps1); //vectorNormal2D(vector3D(gps0, gps1));//
+		double B = Math.sqrt(C*C-A*A);
+		arr[1] = Math.toDegrees(Math.atan(A/B));
+		arr[2] = distance3d(gps0, gps1);
+		System.out.println("Tan-1: "+ Math.toDegrees(Math.atan(A/B))+" | Sin-1: " + Math.toDegrees(Math.asin(A/C)));
+		System.out.println("angle:" +Math.toDegrees(test(gps0).angleZ(test(gps1))));
+
+
+
+
+		return arr;
+	}
 
 	@Override
 	public boolean isValid_GPS_Point(Point3D p) { // works (?)
@@ -161,16 +105,33 @@ public class MyCoords implements coords_converter{
 		// if lat is out of range of (-180, 180) than it's an invalid GPS point
 		if(p.x() < -180 && p.x() > 180)
 			return false;
-		
+
 		// if lon is out of range of (-90, 90) than it's an invalid GPS point
 		if(p.y() < -90 && p.y() > 90)
 			return false;
-		
+
 		// if alt is out of range of (-450, +inf) than it's an invalid GPS point
 		if(p.z() < -450)
 			return false;
-		
+
 		return true;
+	}
+
+
+	
+	public Point3D test(Point3D gps0) {
+	
+		double x = earth_radius*Math.sin(Point3D.d2r(gps0.x()));
+		double y = earth_radius*earth_lon_norm*Math.sin(Point3D.d2r(gps0.y()));
+		
+		return new Point3D(x,y,gps0.z());
+	}
+	
+	public Point3D test2(Point3D v3d) {
+		double x = Math.toDegrees(Math.asin(v3d.x()/earth_radius));
+		double y = Math.toDegrees(Math.asin(v3d.y()/(earth_radius*earth_lon_norm)));
+		
+		return new Point3D(x,y,v3d.z());
 	}
 
 }
