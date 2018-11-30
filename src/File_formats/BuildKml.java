fdde7712c60a8c07179efe83e0216524889965d9
lichy24 <adi_lichy@yahoo.com>
2018-11-30 19:14:27 +0200

    patch
    BuildKml:
    added:
    -timestamp tag
    -javadoc
    -set color by height
    
    MetaData:
    add:
    -frequency as new field of data
    edit:
    -javadoc

--- src/File_formats/BuildKml.java
+++ src/File_formats/BuildKml.java
@@ -2,13 +2,11 @@ package File_formats;
 
 import GIS.*;
 import Geom.Point3D;
-import Geom.Point3D;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -16,7 +14,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.StringWriter;
 import java.util.Iterator;
 
 /**
@@ -29,6 +26,7 @@ public class BuildKml {
     private static Document document;
     private static Element folder;
     private static Iterator<GIS_element> iteratorLayer;
+    private static Point3D beforePoint;
     private static boolean isProject=false;
     
 	/**
@@ -36,7 +34,12 @@ public class BuildKml {
 	 * there should be no instances of this class.
 	 */
     private BuildKml() {}
-    
+
+    /**
+     * create GISProject to kml
+     * @param project GISProject data which to contain in file
+     * @param fileName String String name of file to create and contain layer data
+     */
     public static void create(GIS_project project, String fileName){
         try {
             isProject = true;
@@ -58,15 +61,15 @@ public class BuildKml {
             isProject = false;
         }
     }
-    
+
     /**
-     * 
-     * @param layer
-     * @param fileName
+     * create GISLayer to kml
+     * @param layer GISLayer data which to contain in file
+     * @param fileName String name of file to create and contain layer data
      */
     public static void create(GIS_layer layer, String fileName){
         try {
-            if(!isProject) {
+            if(!isProject) {//if build a project to kml don't run this code
                 factory = DocumentBuilderFactory.newInstance();
                 builder = factory.newDocumentBuilder();
                 document = builder.newDocument();
@@ -77,42 +80,57 @@ public class BuildKml {
                     addGIS((GISElement) iteratorLayer.next());//casting and add
                 }
 
-            if (!isProject)
+            if (!isProject)//if build a project to kml don't run this code
                 createFile(fileName);//create file
             }
             catch (Exception e){
             e.printStackTrace();
         }
     }
+
+    /**
+     * addGIS gets GISElement and create tags to contain its data.
+     * @param gisElement GISElement current element being added to kml
+     */
     private static void addGIS(GISElement gisElement){//add gis element to kml.
         MetaData data = (MetaData)gisElement.getData();
         Point3D geo = (Point3D) gisElement.getGeom();
 
-        Element placemark = document.createElement("Placemark");
+        Element placemark = document.createElement("Placemark");//placemark tag
         folder.appendChild(placemark);
 
-        Element name = document.createElement("name");
+        Element name = document.createElement("name");//name tag
         name.appendChild(document.createCDATASection(data.getSsid()));
         placemark.appendChild(name);
 
-        Element description = document.createElement("description");
-        description.appendChild(document.createCDATASection("BSSID: <b>"+data.getMac()+"</b><br/>Capabilities: <b>"+data.getAuthmode()+"</b><br/>Frequency: <b>"+data.getRssi()+"</b><br/>Timestamp: <b>"+data.getUTC()+"</b><br/>Date: <b>"+data.getFirstseen()+"</b>"));
+        Element description = document.createElement("description");//description tag
+        description.appendChild(document.createCDATASection("BSSID: <b>"+data.getMac()+"</b><br/>Capabilities: <b>"+data.getAuthmode()+"</b><br/>Frequency: <b>"+data.getFrequency()+"</b><br/>Timestamp: <b>"+data.getUTC()+"</b><br/>Date: <b>"+data.getFirstseen()+"</b>"));
         placemark.appendChild(description);
 
-        Element styleUrl = document.createElement("styleUrl");
-        styleUrl.appendChild(document.createTextNode("#red"));
+        Element timestamp = document.createElement("TimeStamp");//timestamp tag
+        placemark.appendChild(timestamp);
+        Element when = document.createElement("when");
+        when.appendChild(document.createTextNode(data.getFirstseen().replace(" ","T")+"Z"));
+        timestamp.appendChild(when);
+
+        Element styleUrl = document.createElement("styleUrl");//styleurl tag
+        styleUrl.appendChild(document.createTextNode(setColorByHeight(geo)));
         placemark.appendChild(styleUrl);
 
         //point
-        Element point = document.createElement("Point");
+        Element point = document.createElement("Point");//point tag
         placemark.appendChild(point);
 
-        Element coordinate = document.createElement("coordinates");
+        Element coordinate = document.createElement("coordinates");//coordinates tag
         point.appendChild(coordinate);
         coordinate.appendChild(document.createTextNode(geo.y()+","+geo.x()));
 
     }
-    private static void setupKml() throws ParserConfigurationException {//kml starter
+
+    /**
+     * setupKml writes in the setup lines of turn xml to kml adding styles etc.
+     */
+    private static void setupKml() {//kml starter
         Element root = document.createElement("kml");
         root.setAttribute("xmlns","http://www.opengis.net/kml/2.2");
         document.appendChild(root);
@@ -123,6 +141,7 @@ public class BuildKml {
         setStyle("red","http://maps.google.com/mapfiles/ms/icons/red-dot.png",doc);//red icon
         setStyle("yellow","http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",doc);//yellow icon
         setStyle("green","http://maps.google.com/mapfiles/ms/icons/green-dot.png",doc);//green icon
+        setStyle("blue","http://maps.google.com/mapfiles/ms/icons/blue-dot.png",doc);//blue icon
 
         folder = document.createElement("Folder");
         doc.appendChild(folder);
@@ -131,6 +150,13 @@ public class BuildKml {
         name.appendChild(document.createTextNode("Wifi Networks"));
         folder.appendChild(name);
     }
+
+    /**
+     *  setStyle build style tags in kml
+     * @param name String tag name
+     * @param iconURL String url of icon image
+     * @param root Element which tag will contain this element.
+     */
     private static void setStyle(String name,String iconURL,Element root){
 
         Element style = document.createElement("Style");
@@ -147,6 +173,12 @@ public class BuildKml {
         href.appendChild(document.createTextNode(iconURL));
         icon.appendChild(href);
     }
+
+    /**
+     * createFile creates file from giving data.
+     * @param fileName String name of file may contain path.
+     * @throws TransformerException in case of failing to create kml file throw exception.
+     */
     private static void createFile(String fileName) throws TransformerException {//setting up kml file to put text into.
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
@@ -157,5 +189,29 @@ public class BuildKml {
         StreamResult streamResult = new StreamResult(new File(fileName));
 
         transformer.transform(domSource,streamResult);
+    }
+
+    /**
+     * setColorByHeight uses difference between current coord and the one before to color icon
+     * @param currentPoint Point3D coords of current gis element
+     * @return return blue if first point/not much difference in height, return red if current point is lower then the one before in quiet a bit
+     *         return green if current point is higher then the one before.
+     */
+    private  static String setColorByHeight(Point3D currentPoint){
+        if (beforePoint ==null){
+            beforePoint = currentPoint;
+            return "#blue";
+        }
+        //System.out.println("before: "+beforePoint.z()+" current: "+currentPoint.z());
+        double diff = beforePoint.z()-currentPoint.z();
+        //System.out.println("diff: "+ diff);
+        beforePoint = currentPoint;
+        if (diff>0.05){
+            return "#red";
+        }
+        if (diff<-0.05){
+            return "#green";
+        }
+        return "#blue";
     }
 }
--- src/GIS/MetaData.java
+++ src/GIS/MetaData.java
@@ -15,18 +15,18 @@ import java.util.TimeZone;
  *
  */
 public class MetaData implements Meta_data {
-    private String mac,ssid,authmode,firstseen,channel,rssi, accuracyMeters,type;
+    private String mac,ssid,authmode,firstseen,channel,rssi, accuracyMeters,type,frequency;
 
     /**
-     * Creates a new MetaData object.
-     * @param mac
-     * @param ssid
-     * @param authmode
-     * @param firstseen
-     * @param channel
-     * @param rssi
-     * @param accuracyMeters
-     * @param type
+     *  Creates a new MetaData object for GISElement.
+     * @param mac String of machine id
+     * @param ssid String display name of device.
+     * @param authmode String type of encryption.
+     * @param firstseen String exact date of device been seen.
+     * @param channel String channel which used for transmission.
+     * @param rssi String Received Signal Strength Indicator show from 0 to 255 the strength of signal the closer to 0 the better.
+     * @param accuracyMeters String the accuracy of device location.
+     * @param type String type of transmission to device.
      */
     public MetaData(String mac, String ssid, String authmode, String firstseen, String channel, String rssi, String accuracyMeters, String type) {
         this.mac = mac;
@@ -37,8 +37,13 @@ public class MetaData implements Meta_data {
         this.rssi = rssi;
         this.accuracyMeters = accuracyMeters;
         this.type = type;
+        setFrequency();
     }
 
+    /**
+     * get UTC by current gis element date
+     * @return long UTC in milliseconds
+     */
     @Override
     public long getUTC() {
         try {
@@ -54,19 +59,87 @@ public class MetaData implements Meta_data {
         return -1;//time cant be negative soo... but should not reach.
     }
 
+    /**
+     * check by type and channel to know what kinda of frequency.
+     */
+    private void setFrequency(){
+        if (type=="GSM")
+            this.frequency = "0";
+        else{
+            this.frequency = String.valueOf(setByChannel());
+        }
+    }
+
+    /**
+     * SetByChannel check by channel of transmission to know what is the center frequency is.
+     * !!! Note this Method only knows channels which serve israel , will not contain channels which not serve this country. !!!
+     * @return int center of frequency.
+     */
+    private int setByChannel(){//get channel set frequency
+        int channelInt = Integer.parseInt(channel);
+        if (channelInt<14&&channelInt>0){
+            return 2407+5*channelInt;
+        }
+        switch (channel){
+            case "34":
+                return 5170;
+            case "36":
+                return 5180;
+            case "38":
+                return  5190;
+            case "40":
+                return  5200;
+            case "44":
+                return  5220;
+            case "46":
+                return  5230;
+            case "48":
+                return 5240;
+            case "54":
+                return  5270;
+            case "62":
+                return  5310;
+            case "56":
+                return 5280;
+            case "60":
+                return  5300;
+            case "64":
+                return 5320;
+            case "52":
+                return  5260;
+        }
+        return 0;
+    }
+
+    /**
+     * get_Orientation not implemented yet.
+     * @return null
+     */
     @Override//TODO need cooords_converter method azimuth_elevation_dist to set orientation.
     public Point3D get_Orientation() {
         return null;
     }
 
+    /**
+     * toString prints all object meta data contains.
+     * @return String of objects in class
+     */
     @Override
     public String toString() {//simple print.
-        return "[ MAC: "+mac+" , SSID: "+ssid+" , AuthMode: "+authmode+" , Firstseen: "+firstseen+" , Channel: "+channel+" , RSSI: "+rssi+" , AccuracyMeters: "+accuracyMeters+" , Type: "+type+" ]";
+        return "[ MAC: "+mac+" , SSID: "+ssid+" , AuthMode: "+authmode+" , Firstseen: "+firstseen+" , Channel: "+channel+", Frequency:"+ frequency+ " , RSSI: "+rssi+" , AccuracyMeters: "+accuracyMeters+" , Type: "+type+" ]";
+    }
+
+    /**
+     * returns this object's frequency
+     * @return String frequency
+     */
+    public String getFrequency() {
+        return frequency;
     }
 
     /**
      * returns this object's mac
-     * @return mac address
+     * @return String mac address
      */
     public String getMac() {
         return mac;
@@ -74,7 +147,7 @@ public class MetaData implements Meta_data {
 
     /**
      * returns this object's SSID
-     * @return SSID address
+     * @return String SSID address
      */
     public String getSsid() {
         return ssid;
@@ -82,39 +155,38 @@ public class MetaData implements Meta_data {
 
     /**
      * returns this Authentication Mode
-     * @return Auth Mode
+     * @return String Auth Mode
      */
     public String getAuthmode() {
         return authmode;
     }
-
     /**
-     * Returns the "firstseen" attribute
-     * @return fristseen
+     * Returns this object's first time seen
+     * @return String firstseen
      */
     public String getFirstseen() {
         return firstseen;
     }
 
     /**
-     * returns the radio channel used
-     * @return channel
+     * returns this object's channel used
+     * @return String channel
      */
     public String getChannel() {
         return channel;
     }
 
     /**
-     * returns RSSI
-     * @return RSSI
+     * returns this object's Received Signal Strength Indicator
+     * @return String RSSI
      */
     public String getRssi() {
         return rssi;
     }
 
     /**
-     * returns the accuracy in meters
-     * @return accuracy
+     * returns this object's accuracy in meters
+     * @return String accuracyMeters
      */
     public String getAccuracyMeters() {
         return accuracyMeters;
@@ -122,7 +194,7 @@ public class MetaData implements Meta_data {
 
     /**
      * returns this object's type
-     * @return type
+     * @return String type
      */
     public String getType() {
         return type;
